7400f0a3e5cd79280e90dfdb208739d1ee0ef91a
ll560 <llavia6@gmail.com>
2021-03-29 14:30:15 -0400

    commit for hw

---
+++ scripts.js
@@ -0,0 +1,49 @@
+//1. Sum of an array
+
+
+// let numberArray = [3, 4, 5];
+// let grandTotal = 0;
+
+// for (let i= 0; i < numberArray.length; i++ ){
+//     grandTotal += numberArray[i];
+// }
+// console.log(grandTotal);
+
+// let numArray = [10, 5, 9]
+// let grandTotal2 = 0;
+
+// for (let i=0; i < numArray.length; i++ ){
+//     grandTotal2 += numArray[i]; 
+// }
+// console.log(grandTotal2);
+
+// //2. Average of an array
+// let array = [3, 4, 5];
+// let avgTotal = 0;
+
+// for (let i = 0; i < array.length; i++){
+//     avgTotal += array[i];
+// }
+
+// avgTotal /= array.length;
+// console.log(avgTotal);
+
+// let array2 = [10, 5, 9];
+// let avgTotal2 = 0;
+
+// for (let i = 0; i < array.length; i++ ){
+//     avgTotal2 += array2[i]
+// }
+// avgTotal2 /= array2.length;
+// console.log(avgTotal2);
+
+//Bonus---------------------
+
+const sum = (arr) => {
+    let total = 0;   
+        for(let i = 0; i > sum.length; i++){
+           
+            total += sumOfArray[i];
+        }
+}
+console.log(sum);
